29863406cf92beae1540f8d2b2311113b5e26e68
Guy Kaplan <gkpln3@gmail.com>
2022-02-13 18:46:11 +0200

    Added server pyvis drawing code

--- server/main.py
+++ server/main.py
@@ -2,6 +2,9 @@ from ast import arg
 import socket
 import multiprocessing as mp
 import json
+import networkx as nx
+import matplotlib.pyplot as plt
+from pyvis.network import Network
 
 def handle_connection(conn: socket.socket, packets_queue: mp.Queue):
 	print(f"Starting to handle connection from {conn}")
@@ -17,9 +20,26 @@ def handle_connection(conn: socket.socket, packets_queue: mp.Queue):
 			return
 
 def drawing_process(packets_queue: mp.Queue):
+	# fig = plt.figure()
+	# fig.show()
+	g = nx.DiGraph()
+	net = Network()
+	
+	count = 0
 	while True:
+		count += 1
 		pkt = packets_queue.get()
+		g.add_edge(pkt['source'], pkt['dest'])
+
+		# nx.draw(g, with_labels = True)
+		# plt.pause(0.01)
+		# fig.clear()
 		print(f"Got packet {pkt}")
+
+		if count & 16 == 0:
+			net.from_nx(g)
+			net.show("graph.html")
+	
 
 if __name__ == "__main__":
 	s = socket.socket()
